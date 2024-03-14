a3560d0adef299bcdea04e85e54bde88fffb41f0
Arnaud Deleruyelle <arnaud.deleruyelle@talan.com>
2024-03-14 13:50:49 +0100

    change code

--- code.py
+++ code.py
@@ -15,10 +15,3 @@ import numpy as np
 data = np.random.randn(100)
 plt.hist(data, bins=20)
 st.pyplot()
-
-# Ajout d'un widget de sélection
-option = st.selectbox(
-    'Quelle est votre couleur préférée ?',
-    ['Rouge', 'Vert', 'Bleu'])
-
-'Vous avez sélectionné :', option
